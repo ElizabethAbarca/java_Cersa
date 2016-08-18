cea787854c67580dd55ab1d59e4c0a7e10595ef5
HP <HP@PC-HP>
2016-08-18 12:04:01 -0500

    controladores para las secciones

---
+++ sg_cersa/src/java/cersa/presentacion/Beans/BCerrarseccion.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cersa.presentacion.Beans;
+
+import cersa.negocio.Clases.CUsuario;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author HP
+ */
+@ManagedBean
+@RequestScoped
+public class BCerrarseccion {
+
+    private CUsuario persona;
+    private final HttpServletRequest httpServletRequest;
+    private final FacesContext faceContext;
+    private FacesMessage facesMessage;
+
+    /**
+     * Creates a new instance of BCerrarseccion
+     */
+    public BCerrarseccion() {
+        faceContext=FacesContext.getCurrentInstance();
+        httpServletRequest=(HttpServletRequest)faceContext.getExternalContext().getRequest();
+        if(httpServletRequest.getSession().getAttribute("session Usuario")!=null)
+        {
+            persona=(CUsuario) httpServletRequest.getSession().getAttribute("session Usuario");
+        }
+    }
+     
+    public String cerrarSession()
+    {
+        httpServletRequest.getSession().removeAttribute("session Usuario");
+        facesMessage=new FacesMessage(FacesMessage.SEVERITY_INFO, "Session cerrada correctamente", null);
+        faceContext.addMessage(null, facesMessage);
+        return "index";
+    }
+    
+}
